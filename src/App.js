59c37f74971388ed0a68cad12b9e350a58c51c9e
Mark Cameron <markcam73@yahoo.com>
2018-01-24 20:54:28 -0500

    jeff's initial changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,15 +7,22 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={"statcalclogo.jpg"} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to stat calc!</h1>
         </header>
+        <body>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         <strong>
+           Let's do all the work for you, Just type in your data
+         </strong>
         </p>
+          <form name = "input" action = "" method = "get">
+            input: <input type= "text" name= "input" value=""></input>
+            <input type= "submit" value="Submit"></input>
+          </form>
+        </body>
       </div>
     );
   }
 }
-
 export default App;
